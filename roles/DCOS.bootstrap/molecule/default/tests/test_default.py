85da365ab2fd6212002a9b1de13451690d326cc1
Marvin Frick <mfrick@mesosphere.com>
2018-10-22 21:20:02 +0200

    fixes Ansible linting issues

--- roles/DCOS.bootstrap/molecule/default/tests/test_default.py
+++ roles/DCOS.bootstrap/molecule/default/tests/test_default.py
@@ -9,8 +9,3 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_install_file(host):
     f = host.file('/opt/dcos-install/2e05da6b45f9f0e486de4d65404a0081099861c8/genconf/serve/dcos_install.sh')
     assert f.exists
-
-
-def test_upgrade_file(host):
-    f = host.file('/opt/dcos-install/2e05da6b45f9f0e486de4d65404a0081099861c8/genconf/serve/upgrade_from_1.10.4/latest/dcos_node_upgrade.sh')
-    assert f.exists
